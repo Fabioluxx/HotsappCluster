fc1b65b9d7c878d9f2fc08988c532c2be9ab65cc
Fabio <fabiolux999@gmail.com>
2020-06-17 21:47:12 -0300

    Update scritps

--- apps/update.bat
+++ apps/update.bat
@@ -1,5 +1,9 @@
 kubectl apply ^
 -f istio/gateway.yaml ^
 -f istio/vservices.yaml ^
--f workloads/nginx.yaml
+-f istio/dest-rules.yaml ^
+-f workloads/appfront.yaml ^
+-f workloads/webapi.yaml ^
+-f workloads/streamer.yaml ^
+-f workloads/playlist-worker.yaml
 PAUSE
