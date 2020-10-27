66031afcecab9b325e58865660888f0266a07b7e
Fabio <fabiolux999@gmail.com>
2020-10-27 01:55:40 -0300

    Edit - Hotsapp WaClient

--- apps/update.bat
+++ apps/update.bat
@@ -1,9 +1,6 @@
 kubectl apply ^
 -f istio/gateway.yaml ^
 -f istio/vservices.yaml ^
--f istio/dest-rules.yaml ^
 -f workloads/appfront.yaml ^
--f workloads/webapi.yaml ^
--f workloads/streamer.yaml ^
--f workloads/playlist-worker.yaml
+-f workloads/webapi.yaml
 PAUSE
